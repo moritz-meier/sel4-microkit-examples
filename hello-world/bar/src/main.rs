6689bfd1f82fdcfa0235b929f90dc6e4fcda1dfc
Moritz Meier <moritz.meier@dlr.de>
2024-11-06 16:36:36 +0100

    wip

---
+++ hello-world/bar/src/main.rs
@@ -0,0 +1,24 @@
+#![no_std]
+#![no_main]
+
+use core::{arch::asm, convert::Infallible, panic::PanicInfo};
+
+use sel4_microkit::{memory_region_symbol, protection_domain, Handler};
+
+struct HandlerImpl;
+
+impl Handler for HandlerImpl {
+    type Error = Infallible;
+}
+
+#[protection_domain]
+fn init() -> impl Handler {
+    sel4_microkit::debug_println!("Hello World!!!!!");
+
+    loop {
+        unsafe { asm!("nop") };
+        sel4_microkit::debug_println!("bar");
+    }
+
+    return HandlerImpl;
+}
